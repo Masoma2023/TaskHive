dee6c7dbe2b723631cb81124b8aea06f0184bcfb
Mohsen Parsa <mohsenp249@gmail.com>
2025-05-13 19:30:53 -0900

    refactor(welcomePage): create darkMode and lightMode in welcome page, welcome page is ready to use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import WelcomePage from '@/components/welcomePage/WelcomePage';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
-      <WelcomePage />
+      Home
     </div>
   );
 }
