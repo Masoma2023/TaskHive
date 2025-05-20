bd369a9bb1160c62b3e07bceb6fb6ac6a9f5ac29
Aliyaser <aliyasermousavi@gmail.com>
2025-05-20 14:10:00 +0430

    fix: improve card components structure and fix layout issues

--- src/components/dashboard/SummaryCards.tsx
+++ src/components/dashboard/SummaryCards.tsx
@@ -7,7 +7,7 @@ import { IoCheckmarkSharp } from 'react-icons/io5';
 export default function SummaryCards() {
   return (
     <>
-      <div className="col-span-1 w-full py-6 pl-4 rounded-[6px] bg-white shadow-2xs">
+      <div className="col-span-1 w-full py-9 pl-6 rounded-[6px] bg-white shadow-md">
         <p className="font-normal -mt-4 text-[#6B7B94] text-[18px] pb-2">Total tasks</p>
         <div className="flex gap-3.5">
           <div className="border w-7 h-7 mt-2 rounded px-1 py-1 flex items-center justify-center border-[#6B7B94]">
@@ -20,7 +20,7 @@ export default function SummaryCards() {
           </div>
         </div>
       </div>
-      <div className="col-span-1 w-full py-6 pl-4 rounded-[6px] bg-white shadow-2xs">
+      <div className="col-span-1 w-full py-9 pl-6 rounded-[6px] bg-white shadow-md">
         <p className="font-normal -mt-4 text-[#6B7B94] text-[18px] pb-2">Done</p>
         <div className="flex gap-3.5">
           <div className="border w-7 h-7 mt-2 rounded px-1 py-1 flex items-center justify-center border-[#6B7B94]">
@@ -31,7 +31,7 @@ export default function SummaryCards() {
           </div>
         </div>
       </div>
-      <div className="col-span-1 w-full py-6 pl-4 rounded-[6px] bg-white shadow-2xs">
+      <div className="col-span-1 w-full py-9 pl-6 rounded-[6px] bg-white shadow-md">
         <p className="font-normal -mt-4 text-[#6B7B94] text-[18px] pb-2">In process</p>
         <div className="flex gap-3.5">
           <div className="border w-7 h-7 mt-2 rounded px-1 py-1 flex items-center justify-center border-[#6B7B94]">
@@ -44,7 +44,7 @@ export default function SummaryCards() {
           </div>
         </div>
       </div>
-      <div className="col-span-1 w-full py-6 pl-4 rounded-[6px] bg-white shadow-2xs">
+      <div className="col-span-1 w-full py-9 pl-6 rounded-[6px] bg-white shadow-md">
         <p className="font-normal -mt-4 text-[#6B7B94] text-[18px] pb-2">Overdue tasks</p>
         <div className="flex gap-3.5">
           <div className="border w-7 h-7 mt-2 rounded px-1 py-1 flex items-center justify-center border-[#6B7B94]">
